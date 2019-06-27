4480b25f1ce04a0acb82ad2eb9f98525a8fb13d7
Elyh Lapetina <evl12@case.edu>
2019-06-27 08:18:15 -0700

    New request

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-const express = require('express')
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+
 const app = express()
 app.get('/', (req, res) => {
   res.send('HEY!')
 })
+
+app.get('/settings', (req, res) => {
+  res.send('Return settings here...')
+})
+
 app.listen(3000, () => console.log('Server running on port 3000'))
