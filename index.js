c98b9259b0950d6188d77dab01194a5ea4e5ceb2
Elyh Lapetina <evl12@case.edu>
2019-07-25 09:13:50 -0700

    mqtt

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const http = require('http');
 const path = require('path');
 const fs = require('fs');
+var mqtt = require('mqtt');
 
 const app = express()
 app.get('/', (req, res) => {
@@ -10,6 +11,28 @@ app.get('/', (req, res) => {
 
 app.get('/settings', (req, res) => {
   res.json({ a: 1 });
+})
+
+app.get('/test', (req,res) => {
+	svar test = 'test'
+    
+    var client  = mqtt.connect('ec2-54-205-131-65.compute-1.amazonaws.com',{port: 1883, username:'elyh',password:'lapetina' })
+    client.on('connect', function () {
+
+      client.subscribe('test', function (err) {
+
+
+        if (!err) {
+          test = 'success'
+          client.publish('test', 'Hello mqtt')
+        } else {
+          test = 'fail'
+
+        }
+      })
+    })
+
+
 })
 
 app.listen(3000, () => console.log('Server running on port 3000'))
